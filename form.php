dc53396ad1168ac184bc4dfdedbaefc8dbbdccaf
jinmanz <jinman@ualberta.ca>
2016-02-25 10:04:37 -0700

    Added files via upload

---
+++ form.php
@@ -0,0 +1,13 @@
+<form method = "post" action="">
+
+<p> First:<input type="text" name="first" id="first" value=""></p>
+<p> Last:<input type="text" name="last" id="last" value=""></p>
+<p> Email:<input type="text" name="email" id="email" value=""></p>
+<p> Phone:<input type="text" name="phone" id="phone" value=""></p>
+<p> Notes:<textarea cols="50" rows="4" name="notes" id="notes"></textarea></p>
+<input type="hidden" id="id" name="id" value="">
+
+<p> <input type="submit" name="submit" id="submit" value="Save to Database"></p>
+
+
+</form>
